eedcd1bf5afed73f8a6c1d5fae12754f779e200a
Israel <ulises.brizuela2928@alumnos.udg.mx>
2023-03-26 19:46:24 -0600

    agregadas nuevas funciones

--- Act5.py
+++ Act5.py
@@ -2,12 +2,129 @@
 #BRIZUELA ARIAS ULISES ISRAEL
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from collections import deque
+import sys
 
 class Ui_MainWindow(object):
 
+    def __init__(self) -> None:
+        self.memoria = [[1000], [400], [1800], [700], [900], [1200], [1500]]
+        self.procesos = []
+        self.indice = 0
+
+    def archivo(self):
+        with open('./archivos.txt', 'r') as archivos:
+            for archivo in archivos.readlines():
+                nombre, tamaño = archivo.strip().split(',')
+                self.procesos.append([nombre, int(''.join(filter(str.isdigit, tamaño)))])
+
+    def primera(self):
+        self.reinicio()
+        for proceso in self.procesos:
+            for i, element in enumerate(self.memoria):
+                if proceso[1] < element[0]:
+                    self.memoria[i][0] -= proceso[1]
+                    self.memoria[i].append(proceso)
+                    break
+        self.gradiente()
+
+    def mejor(self):
+        self.reinicio()
+        for proceso in self.procesos:
+            mejor = None
+            for i, elemento in enumerate(self.memoria):
+                if proceso[1] <=elemento[0] and (mejor is None or elemento[0] < self.memoria[mejor][0]):
+                    mejor = i
+            
+            if mejor is not None:
+                self.memoria[mejor][0] -= proceso[1]
+                self.memoria[mejor].append(proceso)
+        self.gradiente()
+
+    def peor(self):
+        self.reinicio()
+        for proceso in self.procesos:
+            mayor = 0
+            indice = None
+            for i, elemento in enumerate(self.memoria):
+                if proceso[1] < elemento[0] and elemento[0] > mayor:
+                    mayor = elemento[0]
+                    indice = i
+            
+            if indice is not None:
+                self.memoria[indice][0] -= proceso[1]
+                self.memoria[indice].append(proceso)
+        self.gradiente()
+
+
+    def siguiente(self):
+        self.reinicio()
+        for proceso in self.procesos:
+            for i in range(len(self.memoria)):
+                indice_actual = (i + self.indice) % len(self.memoria)
+                bloque = self.memoria[indice_actual]
+            
+                if proceso[1] <= bloque[0]:
+                    self.memoria[indice_actual][0] -= proceso[1]
+                    self.memoria[indice_actual].append(proceso)
+                    self.indice = indice_actual
+                    break
+        self.gradiente()
 
     ###############################################  GUI  ##################################################################
+    def gradiente(self): 
+        for i, elemento in enumerate(self.memoria):
+            text = ""
+            for archivo in elemento[1:]:
+                text += archivo[0] + ' = ' + str(archivo[1]) + ' Kb.\n'
+            text += str(elemento[0]) + ' Kb Libres.'
+            if i == 0 and len(elemento)>1:  
+                g = elemento[0]/1000  
+                self.L_1000.setText(text)
+                self.L_1000.setStyleSheet(f"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:{g} rgba(170, 255, 255, 100), stop:{g+0.0001} rgba(0, 255, 127, 100));")
+            elif i == 1 and len(elemento)>1: 
+                g = elemento[0]/400       
+                self.L_400.setText(text)
+                self.L_400.setStyleSheet(f"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:{g} rgba(170, 255, 255, 100), stop:{g+0.0001} rgba(0, 255, 127, 100));")
+            elif i == 2 and len(elemento)>1:
+                g = elemento[0]/1800        
+                self.L_1800.setText(text)
+                self.L_1800.setStyleSheet(f"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:{g} rgba(170, 255, 255, 100), stop:{g+0.0001} rgba(0, 255, 127, 100));")
+            elif i == 3 and len(elemento)>1:
+                g = elemento[0]/700        
+                self.L_700.setText(text)
+                self.L_700.setStyleSheet(f"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:{g} rgba(170, 255, 255, 100), stop:{g+0.0001} rgba(0, 255, 127, 100));")
+            elif i == 4 and len(elemento)>1:
+                g = elemento[0]/900        
+                self.L_900.setText(text)
+                self.L_900.setStyleSheet(f"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:{g} rgba(170, 255, 255, 100), stop:{g+0.0001} rgba(0, 255, 127, 100));")
+            elif i == 5 and len(elemento)>1:
+                g = elemento[0]/1200        
+                self.L_1200.setText(text)
+                self.L_1200.setStyleSheet(f"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:{g} rgba(170, 255, 255, 100), stop:{g+0.0001} rgba(0, 255, 127, 100));")
+            elif i == 6 and len(elemento)>1:
+                g = elemento[0]/1500        
+                self.L_1500.setText(text)
+                self.L_1500.setStyleSheet(f"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:{g} rgba(170, 255, 255, 100), stop:{g+0.0001} rgba(0, 255, 127, 100));")      
+
+    def reinicio(self):
+        self.memoria = [[1000], [400], [1800], [700], [900], [1200], [1500]]
+        self.indice = 0
+        self.L_1000.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
+        self.L_1000.setText("1000 Kb")
+        self.L_400.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
+        self.L_400.setText("400 Kb")
+        self.L_1800.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
+        self.L_1800.setText("1800 Kb")
+        self.L_700.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
+        self.L_700.setText("700 Kb")
+        self.L_900.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
+        self.L_900.setText("900 Kb")
+        self.L_1200.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
+        self.L_1200.setText("1200 Kb")
+        self.L_1500.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
+        self.L_1500.setText("1500 Kb")
+        
     def font_button(self):
         font = QtGui.QFont()
         font.setPointSize(14)
@@ -20,6 +137,7 @@ class Ui_MainWindow(object):
         font.setPointSize(8)
         font.setBold(True)
         return font
+    
     
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -58,7 +176,7 @@ class Ui_MainWindow(object):
         self.L_700.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
         self.L_700.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.L_700.setFont(self.font_label())
-        self.L_700.setText(" 700 Kb")
+        self.L_700.setText("700 Kb")
         self.L_700.setAlignment(QtCore.Qt.AlignCenter)
 
         self.L_900 = QtWidgets.QLabel(self.centralwidget)
@@ -69,13 +187,13 @@ class Ui_MainWindow(object):
         self.L_900.setText("900 Kb")
         self.L_900.setAlignment(QtCore.Qt.AlignCenter)
 
-        self.L_1350 = QtWidgets.QLabel(self.centralwidget)
-        self.L_1350.setGeometry(QtCore.QRect(0, 574, 350, 146))
-        self.L_1350.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
-        self.L_1350.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.L_1350.setFont(self.font_label())
-        self.L_1350.setText("1350 Kb")
-        self.L_1350.setAlignment(QtCore.Qt.AlignCenter)
+        self.L_1200 = QtWidgets.QLabel(self.centralwidget)
+        self.L_1200.setGeometry(QtCore.QRect(0, 574, 350, 146))
+        self.L_1200.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
+        self.L_1200.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.L_1200.setFont(self.font_label())
+        self.L_1200.setText("1200 Kb")
+        self.L_1200.setAlignment(QtCore.Qt.AlignCenter)
 
         self.L_1500 = QtWidgets.QLabel(self.centralwidget)
         self.L_1500.setGeometry(QtCore.QRect(0, 720, 350, 185))
@@ -105,16 +223,28 @@ class Ui_MainWindow(object):
         self.B_siguiente.setFont(self.font_button())
         self.B_siguiente.setText("SIGUIENTE AJUSTE")
 
+        #self.B_reinicio = QtWidgets.QRadioButton(self.centralwidget)
+        #self.B_reinicio.setGeometry(QtCore.QRect(360, 520, 260, 20))
+        #self.B_reinicio.setFont(self.font_button())
+        #self.B_reinicio.setText("REINICIO MEMORIA")
+
         MainWindow.setCentralWidget(self.centralwidget)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-# COLOR GRADIAL PARA TAMAÑO DE ARCHIVO background-color: qlineargradient(spread:pad, x1:0.005, y1:0, x2:1, y2:0, stop:0.5 rgba(0, 255, 127, 100), stop:0.5001 rgba(170, 255, 255, 100));
+####################### LLAMADAS A FUCNIONES #########################################
+
+        self.B_primer.toggled.connect(self.primera)
+        self.B_mejor.toggled.connect(self.mejor)
+        self.B_peor.toggled.connect(self.peor)
+        self.B_siguiente.toggled.connect(self.siguiente)
+        self.B_reinicio.toggled.connect(self.reinicio)
+
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
+    ui.archivo()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
