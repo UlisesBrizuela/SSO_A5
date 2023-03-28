aa531b0e66294eb7b10c8e3c2d2cea1734f867e6
Israel <ulises.brizuela2928@alumnos.udg.mx>
2023-03-27 20:19:27 -0600

    eliminacion de codigo comentado

--- Act5.py
+++ Act5.py
@@ -13,12 +13,14 @@ class Ui_MainWindow(object):
         self.procesos = []
         self.indice = 0
 
+#   LEE EL ARCHIVO DE TEXTO Y TRAE A UNA LISTA SUS ELEMENTOS 
     def archivo(self):
         with open('./archivos.txt', 'r') as archivos:
             for archivo in archivos.readlines():
                 nombre, tamaño = archivo.strip().split(',')
                 self.procesos.append([nombre, int(''.join(filter(str.isdigit, tamaño)))])
 
+#   ALGORITMOS DE ASEIGNACION DE MEMORIA
     def primera(self):
         self.reinicio()
         for proceso in self.procesos:
@@ -113,7 +115,7 @@ class Ui_MainWindow(object):
                 self.L_1500.setText(text)
                 self.L_1500.setStyleSheet(f"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:{g} rgba(170, 255, 255, 100), stop:{g+0.0001} rgba(0, 255, 127, 100));")      
 
-# REINICIA LOS VALORES DE LA MEMORIA Y LOS COLORES DE LOS LABELS A SUS PREDETERMINADOS
+#   REINICIA LOS VALORES DE LA MEMORIA Y LOS COLORES DE LOS LABELS A SUS PREDETERMINADOS
     def reinicio(self):
         self.memoria = [[1000], [400], [1800], [700], [900], [1200], [1500]]
         self.indice = 0
@@ -148,6 +150,8 @@ class Ui_MainWindow(object):
     
 #   CONFIGURACION GENERAL DE LA INTERFAZ   
     def setupUi(self, MainWindow):
+
+#       CREACION DE NUESTRA VENTANA PRINCIPAL Y SUS ATRIBUTOS
         MainWindow.setObjectName("MainWindow")
         MainWindow.setFixedSize(600, 905)
         MainWindow.setWindowTitle("ACT5: ADMINISTRADOR DE MEMORIA 1")
@@ -155,82 +159,45 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-
-
-
-        #self.L_1000, self.L_1200, self.L_1500, self.L_1800, self.L_400, self.L_700, self.L_900 = QtWidgets.QLabel(self.centralwidget)
+#       CREACION DE LABELS Y SE ESTABLECE SU TEXTO Y POSISION UNICOS
         self.L_1000 = QtWidgets.QLabel(self.centralwidget)
         self.L_1000.setGeometry(QtCore.QRect(0, 0, 350, 120))
-        #self.L_1000.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
-        #self.L_1000.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.L_1000.setFont(self.font_label())
         self.L_1000.setText("1000 Kb")
-        #self.L_1000.setAlignment(QtCore.Qt.AlignCenter)
         
         self.L_400 = QtWidgets.QLabel(self.centralwidget)
         self.L_400.setGeometry(QtCore.QRect(0, 120, 350, 42))
-        #self.L_400.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
-        #self.L_400.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.L_400.setFont(self.font_label())
         self.L_400.setText("400 Kb")
-        #self.L_400.setAlignment(QtCore.Qt.AlignCenter)
 
         self.L_1800 = QtWidgets.QLabel(self.centralwidget)
         self.L_1800.setGeometry(QtCore.QRect(0, 162, 350, 224))
-        #self.L_1800.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
-        #self.L_1800.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.L_1800.setFont(self.font_label())
         self.L_1800.setText("1800 Kb")
-        #self.L_1800.setAlignment(QtCore.Qt.AlignCenter)
 
         self.L_700 = QtWidgets.QLabel(self.centralwidget)
         self.L_700.setGeometry(QtCore.QRect(0, 386, 350, 81))
-        #self.L_700.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
-        #self.L_700.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.L_700.setFont(self.font_label())
         self.L_700.setText("700 Kb")
-        #self.L_700.setAlignment(QtCore.Qt.AlignCenter)
 
         self.L_900 = QtWidgets.QLabel(self.centralwidget)
         self.L_900.setGeometry(QtCore.QRect(0, 467, 350, 107))
-        #self.L_900.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
-        #self.L_900.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.L_900.setFont(self.font_label())
         self.L_900.setText("900 Kb")
-        #self.L_900.setAlignment(QtCore.Qt.AlignCenter)
 
         self.L_1200 = QtWidgets.QLabel(self.centralwidget)
         self.L_1200.setGeometry(QtCore.QRect(0, 574, 350, 146))
-        #self.L_1200.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
-        #self.L_1200.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.L_1200.setFont(self.font_label())
         self.L_1200.setText("1200 Kb")
-        #self.L_1200.setAlignment(QtCore.Qt.AlignCenter)
 
         self.L_1500 = QtWidgets.QLabel(self.centralwidget)
         self.L_1500.setGeometry(QtCore.QRect(0, 720, 350, 185))
-        #self.L_1500.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
-        #self.L_1500.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #self.L_1500.setFont(self.font_label())
         self.L_1500.setText("1500 Kb")
-        #self.L_1500.setAlignment(QtCore.Qt.AlignCenter)
 
+#       DECLARACION DE LOS ATRIBUTOS VISUALES COMPARTIDOS DE TODOS LOS LABELS
         Labels = [self.L_1000, self.L_1200, self.L_1500, self.L_1800, self.L_400, self.L_700, self.L_900]
         for label in Labels:
-            #label.QtWidgets.QLabel(self.centralwidget)
             label.setStyleSheet("background-color: rgba(170, 255, 255, 100);")
             label.setFrameShape(QtWidgets.QFrame.StyledPanel)
             label.setFont(self.font_label())
             label.setAlignment(QtCore.Qt.AlignCenter)
 
 
-
-
-
-
-
-
-
+#      DECLARACION DE LOS BOTONES, SU POSICION Y ATRIBUTOS DE TEXTO
         self.B_primer = QtWidgets.QRadioButton(self.centralwidget)
         self.B_primer.setGeometry(QtCore.QRect(360, 400, 260, 20))
         self.B_primer.setFont(self.font_button())
@@ -254,13 +221,14 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-####################### LLAMADAS A FUCNIONES #########################################
+###########################################################################################################################################################################
+############################################################## LLAMADAS A FUCNIONES #######################################################################################
 
+#       CUANDO SE SELECCIONA UNO DE LOS BOTONES, SE MANDA A LLAMAR A CADA UNA DE LAS FUNCIONES
         self.B_primer.toggled.connect(self.primera)
         self.B_mejor.toggled.connect(self.mejor)
         self.B_peor.toggled.connect(self.peor)
         self.B_siguiente.toggled.connect(self.siguiente)
-
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
